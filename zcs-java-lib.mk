12d44991c4ee32078c59a18a02a1d30fa2326d22
moonshine git master <enrico.weigelt@vnc.biz>
2012-04-20 23:05:18 +0200

    also installing to lib/jars/

--- zcs-java-lib.mk
+++ zcs-java-lib.mk
@@ -1,42 +1,49 @@
 
 include $(TOPDIR)/conf.mk
 
+ifeq ($(ZCS_LIB_JARS),)
 IMPORT_CP=`[ -d lib ] && find lib -name "*.jar" -exec "echo" "-n" "{}:" ";"`
+else
+IMPORT_ZCS=$(addprefix $(ZIMBRA_BUILD_ROOT)/lib/jars/,$(ZCS_LIB_JARS))
+IMPORT_CP=`[ -d lib ] && find lib -name "*.jar" -exec "echo" "-n" "{}:" ";" ; find $(IMPORT_ZCS) -exec "echo" "-n" "{}:" ";"`
+endif
+
 SRCS=`find -L src -name "*.java"`
 
 ZIMLET_USER_JARDIR=mailboxd/webapps/zimbra/WEB-INF/lib
 ZIMLET_ADMIN_JARDIR=mailboxd/webapps/zimbraAdmin/WEB-INF/lib
 ZIMLET_SERVICE_JARDIR=mailboxd/webapps/service/WEB-INF/lib
+ZIMLET_LIB_JARDIR=lib/jars
 
-all:	build
+build:	check-1	install
 
-build:	install_user install_admin install_service
+ifeq ($(ZIMBRA_BUILD_ROOT),)
+ZIMBRA_BUILD_ROOT=$(HOME)
+check-1:
+	@echo
+	@echo "ZIMBRA_BUILD_ROOT is not set. assuming $$HOME"
+	@echo
+else
+check-1:
+	@true
+endif
 
+install:	$(JAR_FILE_NAME)
 ifeq ($(INSTALL_USER),y)
-install_user:	$(JAR_FILE_NAME)
 	@mkdir -p $(IMAGE_ROOT)/$(ZIMLET_USER_JARDIR)
 	@cp $(JAR_FILE_NAME) $(IMAGE_ROOT)/$(ZIMLET_USER_JARDIR)
-else
-install_user:
-	@echo -n
 endif
-
 ifeq ($(INSTALL_ADMIN),y)
-install_admin:	$(JAR_FILE_NAME)
 	@mkdir -p $(IMAGE_ROOT)/$(ZIMLET_ADMIN_JARDIR)
 	@cp $(JAR_FILE_NAME) $(IMAGE_ROOT)/$(ZIMLET_ADMIN_JARDIR)
-else
-install_admin:
-	@echo -n
 endif
-
 ifeq ($(INSTALL_SERVICE),y)
-install_service:	$(JAR_FILE_NAME)
 	@mkdir -p $(IMAGE_ROOT)/$(ZIMLET_SERVICE_JARDIR)
 	@cp $(JAR_FILE_NAME) $(IMAGE_ROOT)/$(ZIMLET_SERVICE_JARDIR)
-else
-install_service:
-	@echo -n
+endif
+ifeq ($(INSTALL_LIB),y)
+	@mkdir -p $(IMAGE_ROOT)/$(ZIMLET_LIB_JARDIR)
+	@cp $(JAR_FILE_NAME) $(IMAGE_ROOT)/$(ZIMLET_LIB_JARDIR)
 endif
 
 clean:
